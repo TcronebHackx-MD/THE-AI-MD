4d58c8a86e71aacdcc146ffcad0df19ab192595d
Tcroneb Hackx <cartoonboyzfamily@gmail.com>
2024-09-30 13:25:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put season id",
+SESSION_ID: process.env.SESSION_ID || "Q6chmYAK#tPzjk5ygFXAYqB8Pc0cTboRnMssTbzK0A4oCgcJzSt0",
 MONGODB: process.env.MONGODB || "mongodb://mongo:MfPJLADhRWNHvxuOORrlmfJHboedaADB@junction.proxy.rlwy.net:57828",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/70ssfa.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi AI-MD V1.9 Is Online Now 💻\n*💻 Owner* - TCRONEB HACKX\n\n*💻 Owner Number* -263716394979",
